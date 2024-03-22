9777ef07ed228b6856261ca1851c1f1ba9a19c0e
syconstantin <syrus.samson99@gmail.com>
2024-03-22 17:18:37 -0400

    updates to node - dynamo SDK integration

--- forum/server/server.mjs
+++ forum/server/server.mjs
@@ -1,4 +1,15 @@
 import express from 'express'
+import AWS from 'aws-sdk'
+
+const accessKeyId = "AKIAZXKYFLQSGIA6F7XV"
+const secretAccessKey = "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"
+
+AWS.config.update({
+  region: 'us-east-2',
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey,
+});
+const dynamo = new AWS.DynamoDB({apiVersion: "2012-08-10"});
 
 const app = express();
 app.use(express.json());
@@ -12,7 +23,26 @@ app.use((req, res, next) => {
 
 app.post('/', (req, res) => {
   console.log(req.body);
-  res.send('POST request received successfully.');
+  const { body } = req;
+
+  const params = {
+    TableName: 'forumPosts',
+    Item: {
+      forumPostID: {S: body.id},
+      date: {N: body.date},
+      title: { S: body.title }, 
+      comment: { S: body.comment },
+      sessionId: {S: body.string },
+      subject: {S: body.subject}
+    },
+  };
+  dynamo.putItem(params, (err, data) => {
+    if (err) {
+      res.send('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
+    } else {
+      res.send('Added item:', JSON.stringify(data, null, 2));
+    }
+  });
 });
 
 app.listen(3001);
